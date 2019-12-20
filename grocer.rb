55f4744ea5c18bf0e0c7302959d2f29aad338cd5
Jared Matta <cherokeedreemer@gmail.com>
2019-12-19 18:12:16 -0700

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,16 +1,27 @@
+require "pry"
 def find_item_by_name_in_collection(name, collection)
+  collection[name]
+
   # Implement me first!
   #
   # Consult README for inputs and outputs
 end
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
+  new_cart = {} 
+  cart.each do |items_array| 
+    items_array.each do |item, attribute_hash| 
+      new_cart[item] ||= attribute_hash 
+      new_cart[item][:count] ? new_cart[item][:count] += 1 :   
+      new_cart[item][:count] = 1 
+  end 
+end 
+new_cart 
+end
+ # Consult README for inputs and outputs
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-end
-
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
@@ -18,6 +29,8 @@ def apply_coupons(cart, coupons)
 end
 
 def apply_clearance(cart)
+ 
+
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
