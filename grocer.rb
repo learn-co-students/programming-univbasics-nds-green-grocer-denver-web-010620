517149f437a1dc002faa131ecffac905aaff5f1f
Dillon Franklin <drfranklin1331@gmail.com>
2019-12-29 00:39:13 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,43 @@
+require 'pp'
+require 'pry'
+
+
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+  i = 0
+    while i < collection.length do 
+      if collection[i][:item] == name 
+         return collection[i]
+      
+     end
+    i += 1
+   end
+ nil
 end
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+ new_cart = []  
+  i = 0
+  
+    while i < cart.length
+      new_cart_item = find_item_by_name_in_collection(cart[i][:item], new_cart) 
+          if new_cart_item != nil
+            new_cart_item[:count] += 1
+          else
+            new_cart_item = {
+              :item => cart[i][:item],
+              :price => cart[i][:price],
+              :clearance => cart[i][:clearance],
+              :count => 1
+            }
+            new_cart << new_cart_item
+     i += 1
+    end
+   nil
+  end
+ new_cart
 end
+
+
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
@@ -18,9 +46,9 @@ def apply_coupons(cart, coupons)
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  
+  
+  
 end
 
 def checkout(cart, coupons)
